59dadb3f61caa07a58b42f7fd2a19063425f1441
tejasdani123 <43982776+tejasdani123@users.noreply.github.com>
2018-11-27 13:19:30 +0530

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,12 @@
+marks=int(input("enter the marks of the student"))
+if marks>=91 and marks<=100:
+	print('grade a')
+if marks>=81 and marks<=90:
+	print('grade b')
+if marks>=71 and marks<=80:
+	print('grade c')
+if marks>=61 and marks<=70:
+	print('grade d')
+if marks>=0 and marks<=60:
+	print('grade f')
+
